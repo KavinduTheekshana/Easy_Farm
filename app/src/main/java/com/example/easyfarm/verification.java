7d0986b9cad652d270b0ef891fafcfb21374c790
KavinduTheekshana <kavindutheekshana@gmail.com>
2022-09-25 17:56:43 +0100

    Verification

---
+++ app/src/main/java/com/example/easyfarm/verification.java
@@ -0,0 +1,14 @@
+package com.example.easyfarm;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class verification extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_verification);
+    }
+}
